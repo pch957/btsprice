99ca5a1063f478d65651742a977e578014d29dcd
alt <pch957@163.com>
2016-01-07 12:51:42 +0800

    new header

--- btsprice/exchanges.py
+++ btsprice/exchanges.py
@@ -8,7 +8,8 @@ class Exchanges():
     def __init__(self):
         header = {
             'content-type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 Gecko/20100101 Firefox/22.0'}
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0'}  # noqa
+
         self.session = aiohttp.ClientSession(headers=header)
         self.order_types = ["bids", "asks"]
 
